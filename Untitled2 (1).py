df344604bb37e752d6777e740e1e97ab87b9457f
AmyrSam <68240352+AmyrSam@users.noreply.github.com>
2020-11-10 11:30:23 +0800

    Add files via upload

---
+++ Untitled2 (1).py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+class Square:
+    side = 1.0
+    
+    def setSide(self, side):
+        self.side = parameter
+        
+        if parameter<0:
+            self.side = 0
+
+
+# In[ ]:
+
+
+
+
